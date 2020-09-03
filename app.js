6a292258f30e9fcb023415cb12965e196ee96bd2
Ryan Warner <ryan.warner.us@gmail.com>
2020-09-03 16:21:36 -0400

    Starting to bring it all together

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const express = require("express");
 const app = express();
 const port = 80;
 
+
+var fs = require("fs");
+
+var directory = fs.readFileSync("directory.json");
+
+var jsonContent = JSON.parse(directory);
+
+console.log("Gallery:", jsonContent[0].contents[0].name);
+console.log("Folder:", jsonContent[0].contents[0].contents[0].name);
+console.log("File:", jsonContent[0].contents[0].contents[0].contents[1].name);
+console.log("File Path:",jsonContent[0].name+'/'+jsonContent[0].contents[0].name+'/'+jsonContent[0].contents[0].contents[0].name+'/')
+
 app.set('view engine', 'pug');
 
 //render homepage
