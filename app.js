c9fc7db3d5518215a684c7dfde627a13102b67f7
Ryan Warner <ryan.warner.us@gmail.com>
2020-12-07 14:44:03 -0500

    Refactored Gallery Indexing Logic

--- app.js
+++ app.js
@@ -17,14 +17,53 @@ const directory = fs.readFileSync("directory.json");
 
 const jsonContent = JSON.parse(directory);
 
-console.log("Gallery:", jsonContent[0].contents[0].name);
-console.log("Folder:", jsonContent[0].contents[0].contents[0].name);
-console.log("File:", jsonContent[0].contents[0].contents[0].contents[1].name);
-console.log("File Path:",jsonContent[0].name+'/'+jsonContent[0].contents[0].name+'/'+jsonContent[0].contents[0].contents[0].name+'/')
+// console.log("Gallery:", jsonContent[0].contents[0].name);
+// console.log("Folder:", jsonContent[0].contents[0].contents[0].name);
+// console.log("File:", jsonContent[0].contents[0].contents[0].contents[1].name);
+// console.log("File Path:",jsonContent[0].name+'/'+jsonContent[0].contents[0].name+'/'+jsonContent[0].contents[0].contents[0].name+'/')
 
-var navigation =  jsonContent[0].contents[0].contents;
+console.log('Initializing Photo Gallery')
+console.log(' ')
 
-console.log("Reading the index...")
+
+var galleryRoot = jsonContent[0].name
+console.log('Gallery Root: ' + galleryRoot)
+console.log(' ')
+
+// finding HQ/SQ/T Gallery Indices
+for (var i = 0; i <= jsonContent.length; i++){
+  if (jsonContent[0].contents[i].name == 'HQ'){
+      var HQInd = i
+      console.log('High Quality Directory Found at index ' + HQInd)
+  }
+}
+
+for (var i = 0; i <= jsonContent.length; i++){
+    if (jsonContent[0].contents[i].name == 'SQ'){
+        var SQInd = i
+        console.log('Standard Quality Directory Found at index ' + SQInd)
+    }
+  }
+
+  for (var i = 0; i <= jsonContent.length; i++){
+    if (jsonContent[0].contents[i].name == 'T'){
+        var ThumbInd = i
+        console.log('Thumbnail Directory Found at index ' + ThumbInd)
+    }
+  }
+
+var navigation =  jsonContent[0].contents[`${ThumbInd}`].contents;
+var hQPath = galleryRoot + '/' + jsonContent[0].contents[HQInd].name
+var sQPath = galleryRoot + '/' + jsonContent[0].contents[SQInd].name
+var thumbPath = galleryRoot + '/' + jsonContent[0].contents[ThumbInd].name
+
+console.log(' ')
+console.log('Library Paths')
+console.log('High Quality: ' + hQPath)
+console.log('Standard Quality: ' + sQPath)
+console.log('Thumbnail: ' + thumbPath)
+
+console.log(' ')
 
 var navigationArray = [];
 var linkArray = [];
@@ -36,10 +75,13 @@ navigation.forEach(element => {
     //convert to usable name, replacing three hyphens if they occur together with a single hyphen
     usableName = element.name.replace(/\s+/g, '-').toLowerCase();
     usableName = usableName.replace('---', '-');
-    console.log("   "+"Usable Name: "+usableName);
+    console.log("   "+"Usable Name: " + usableName);
+
+    var currentPath = thumbPath + '/' + element.name
+    console.log('   ' + 'Current Path: ' + currentPath)
     
     //calculate the number of rows and columns
-    console.log(element)
+    // console.log(element)
     
     //need to set a foreach in this, add name to array (skip zone identifier?) then pass that array to the pug page
     //the pug array should calculate the length, divide by 5, and round up to find the number of rows
@@ -47,7 +89,7 @@ navigation.forEach(element => {
     //at least just HQ/SQ/T
 
     //my attempt to serve the directory (didn't really work)
-    app.use(express.static(__dirname));
+    app.use('/' + usableName + '-database',express.static(currentPath));
 
 
 
@@ -55,9 +97,10 @@ navigation.forEach(element => {
     app.get("/"+`${usableName}`, (req, res) => {
         res.render('gallery', {
             title: `${element.name}`,
+            usableName: `${usableName}-database`,
             //trying to pass the path through, need to figure out how
             //likely need to add different elements of the JSON together
-            path: `${element}`
+            path: `${currentPath}`
 
         });
     });
@@ -81,8 +124,9 @@ app.get("/navigation", (req, res) => {
     });
 });
 
-//...
 
 app.listen(port, function() {
+    console.log(' ')
+    console.log('Indexing Completed')
     console.log(`Server listening on port ${port}`);
 });
