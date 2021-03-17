f5f7a2ca6d3a4c759cdb28e09984b5793b4d3fb7
Ryan Warner <rwarner31@gatech.edu>
2021-03-16 20:52:13 -0400

    Updated some stying for navigation pages

--- js/index.js
+++ js/index.js
@@ -1,32 +1,34 @@
 function fitFill() {
 
-    var temp = document.querySelector('.images')
+    var array = document.querySelectorAll('[id=images]');
 
-    var all = document.getElementsByClassName('images');
-
-    var style = window.getComputedStyle(temp, null).getPropertyValue('transform');
+    var style = getComputedStyle(array[0], null).getPropertyValue('--tw-scale-x');
     
     var length = style.length;
+    console.log('Test: ' + style)
+    console.log(length)
 
-    if(length == 24) {
+    if(length == 1) {
 
         document.getElementById('fitFill').src='/assets/fit.svg';
         document.getElementById('fitFillText').innerHTML = 'Fill';
 
-        for(var i = 0; i < all.length; i++){
-            var image = all[i];
-            image.style.transform = 'scale(1.6)';
+        for(var i = 0; i < array.length; i++){
+            var image = array[i];
+            image.style[--tw-scale-x] = 1.6;
+            image.style[--tw-scale-x] = 1.6;
         };
     }
 
-    if(length == 28) {
+    if(length == 3) {
 
         document.getElementById('fitFill').src='/assets/fill.svg';
         document.getElementById('fitFillText').innerHTML = 'Fit';
 
-        for (var i = 0; i < all.length; i++){
-            var image = all[i];
-            image.style.transform = 'scale(1.0)';
+        for (var i = 0; i < array.length; i++){
+            var image = aray[i];
+            image.style.transform = 'tw-scale-x(1.0)';
+            image.style.transform = 'tw-scale-y(1.0)';
         };
     }
 }
